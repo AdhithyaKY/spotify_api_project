ba565d93995ba79e22f3269e3d65bbf518b4473d
Andrew Peterson <andrewp2@umbc.edu>
2021-12-23 01:31:38 -0500

    basic home/about testing

--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'blog post 1',
+        'content': 'first post',
+        'date_posted': 'august 27, 2018'
+    },
+    {
+        'author': 'jane doe',
+        'title': 'blog post 2',
+        'content': '2nd post',
+        'date_posted': 'august 27, 2018'
+    }
+]
+
 
 def home(request):
-    return render(request, 'landing/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'landing/home.html', context)
 
 
 def about(request):
-    return render(request, 'landing/about.html')
+    return render(request, 'landing/about.html', {'title': 'about'})
