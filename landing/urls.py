a6530b5dc98a973d29024b5b2887cae120080042
Andrew Peterson <andrewp2@umbc.edu>
2021-12-23 16:53:11 -0500

    adding templates, new page dedicated for stats

--- landing/urls.py
+++ landing/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='landing-home'),
     path('about/', views.about, name='landing-about'),
+    path('stats.html', views.stats, name='landing-stats'),
 ]
