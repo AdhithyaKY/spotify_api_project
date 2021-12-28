fc09ed31e9d9bf42074726f4586500baad1d3886
AdhithyaKY <AdhithyaKY@gmail.com>
2021-12-27 20:15:23 -0500

    modified settings to allow for static files.

--- spotify_api_project/settings.py
+++ spotify_api_project/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 ]
 
 MIDDLEWARE = [
@@ -127,6 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 STATIC_URL = '/static/'
 
