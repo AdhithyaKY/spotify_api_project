fe8f72d7a1f119a9c768e28ec2a318459e0b597f
Andrew Peterson <andrewp2@umbc.edu>
2021-12-30 10:10:22 -0500

    change submit button color for mismatched password

--- users/static/users/password-validation.js
+++ users/static/users/password-validation.js
@@ -1,7 +1,9 @@
 window.onkeyup = function validate_password() {
     if (document.getElementById('id_password1').value == document.getElementById('id_password2').value) {
+        document.getElementById('submit-button').className = 'btn btn-outline-primary';
         document.getElementById('submit-button').disabled = false;
     } else {
+        document.getElementById('submit-button').className = 'btn btn-outline-danger';
         document.getElementById('submit-button').disabled = true;
     }
 }
