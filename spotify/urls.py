76ba4fa5d68e0cf1cd66a2f48ef61c625ab480e9
AdhithyaKY <AdhithyaKY@gmail.com>
2022-01-07 23:27:47 -0500

    added functionality to send requests to any spotify api endpoint.  Added view for retrieiving top artists, but it currently returns raw json data returned by api call.  Need to check if tokens need to be refreshed before making any api calls.

--- spotify/urls.py
+++ spotify/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AuthorizationURL, RequestAccessToken, IsAuthenticated, remove_user_token
+from .views import AuthorizationURL, GetUserTopArtists, RequestAccessToken, IsAuthenticated, remove_user_token
 
 urlpatterns = [
     path('get-authorization-url/', AuthorizationURL.as_view(), name='spotify-auth'),
     path('redirect/', RequestAccessToken.as_view(), name='spotify-redirect'),
     path('is_authenticated/', IsAuthenticated.as_view(), name='spotify-auth-check'),
     path('remove_auth/', remove_user_token, name='spotify-remove-auth'),
+    path('get_user_top_artists/', GetUserTopArtists.as_view(), name="spotify-user-top-artists")
 ]
