96579a2d9adbc6eaab6ddb961b958917df769e46
Andrew Peterson <andrewp2@umbc.edu>
2022-01-07 20:05:47 -0500

    local redirect

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -3,6 +3,6 @@ import os
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 # if in aws, change to:
-REDIRECT_URI = "http://ec2-54-172-154-161.compute-1.amazonaws.com:8000/spotify/redirect/"
+#REDIRECT_URI = "http://ec2-54-172-154-161.compute-1.amazonaws.com:8000/spotify/redirect/"
 # if testing locally, change to:
-#REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect/"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect/"
